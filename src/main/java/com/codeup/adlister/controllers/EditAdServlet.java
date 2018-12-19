e883b37ded380395156df28e76f8cc9ac4bcd051
Stuart Hamblin <stuart@Stuarts-MacBook-Air.local>
2018-12-19 17:01:55 -0600

    edit-ad front end complete

--- src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
@@ -1,5 +1,8 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,12 +10,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "EditAdServlet", urlPatterns = "/ad/edit")
+@WebServlet(name = "EditAdServlet", urlPatterns = "/ads/edit")
 public class EditAdServlet extends HttpServlet {
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/ads/edit-ad.jsp").forward(request, response);;
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Long ad_id = Long.parseLong(request.getParameter("ad_id"));
+        Ad ad = DaoFactory.getAdsDao().findAdById(ad_id);
+        request.getSession().setAttribute("ad_id", ad.getId());
+        request.getSession().setAttribute("ad_title", ad.getTitle());
+        request.getSession().setAttribute("ad_desc", ad.getDescription());
+        response.sendRedirect("/ads/edit");
+//        request.getRequestDispatcher("/WEB-INF/ads/edit-ad.jsp").forward(request, response);;
     }
 }
